a69398d91eab67a9bd94ddab835f62aecaf2f023
DanSager <sagerdanw@gmail.com>
2019-10-10 19:44:11 -0400

    Logout button in Navbar signs the user out and returns them to the login page #41

--- frontend/app/containers/Pages/Login/Login.js
+++ frontend/app/containers/Pages/Login/Login.js
@@ -9,7 +9,11 @@ firebase.initializeApp({
 })
 class Login extends Component {
 
-    state={isSignedIn: false}
+    state = {
+      isSignedIn: false,
+      loading: false,
+      isAuthenticating: true,
+    }
 
     uiConfig = {
       signInFlow: "popup",
@@ -22,38 +26,56 @@ class Login extends Component {
     }
 
     componentDidMount = ()=>{
+      this.authUser().then((user) => {
+        this.setState({ isAuthenticating: false });
+     }, (error) => {
+        this.setState({ isAuthenticating: false });
+        alert(e);
+     });
+
       firebase.auth().onAuthStateChanged(user => {
         this.setState({isSignedIn:!!user})
-        console.log("user",user)
         if (user) {
           window.location.href = '/dashboard';
         }
       })
     }
 
+    authUser() {
+      return new Promise(function (resolve, reject) {
+         firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+              resolve(user);
+              window.location.href = '/dashboard';
+              this.setState({isSignedIn:!!user})
+            } else {
+              reject('User not logged in');
+            }             
+         });
+      });
+    }
+
     render() {
-      if (this.state.loading) {
+      if (this.state.loading || this.state.isAuthenticating) {
         return <Loading />;
       }
+      //window.location.pathname = '/dashboard'
+            //  <span>
+            //  <div>Signed In!</div>
+            //  <button onClick={()=>firebase.auth().signOut()}>Sign Out</button>
+            //  <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            //  <h1>Email: {firebase.auth().currentUser.email}</h1>
+            //  <img alt="profile picture" src={firebase.auth().currentUser.photoURL}/>
+            //</span>
       return (
-        <div className="Login">
-          {this.state.isSignedIn ? (
-            //window.location.pathname = '/dashboard'
-              <span>
-              <div>Signed In!</div>
-              <button onClick={()=>firebase.auth().signOut()}>Sign Out</button>
-              <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-              <h1>Email: {firebase.auth().currentUser.email}</h1>
-              <img alt="profile picture" src={firebase.auth().currentUser.photoURL}/>
-            </span>
-        ) : (
+        <div className="Login">(
+            
           <span><h1>Welcome to Django Mango</h1>
             <StyledFirebaseAuth
               uiConfig={this.uiConfig}
               firebaseAuth={firebase.auth()}
             />
           </span>
-        )}
       </div>
     );
   }
